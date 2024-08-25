a2e0ea46bb55930b85dacca1633285ca985605fc
happo31 <happo31@gmail.com>
2024-08-25 16:19:34 +0000

    feat: zod-openapi で書き直し

--- packages/core/src/type.ts
+++ packages/core/src/type.ts
@@ -1,51 +1,65 @@
-export type Album = {
-  cid: string;
-  name: string;
-  coverUrl: string;
-  artistes: string[];
-};
+import { z } from "npm:@hono/zod-openapi";
 
-export type SongSummary = {
-  cid: string;
-  name: string;
-  artistes: string[];
-};
+export const AlbumSchema = z.object({
+  cid: z.string(),
+  name: z.string(),
+  coverUrl: z.string(),
+  artistes: z.array(z.string()),
+});
 
-export type AlbumDetails = {
-  cid: string;
-  name: string;
-  intro: string;
-  belong: string;
-  coverUrl: string;
-  coverDeUrl: string;
-  songs: SongSummary[];
-};
+export const SongSummarySchema = z.object({
+  cid: z.string(),
+  name: z.string(),
+  artistes: z.array(z.string()),
+});
 
-export type Song = {
-  cid: string;
-  name: string;
-  albumCid: string;
-  sourceUrl: string;
-  lyricUrl: string | null;
-  mvUrl: string | null;
-  mvCoverUrl: string | null;
-  artists: string[];
-};
+export const AlbumDetailsSchema = z.object({
+  cid: z.string(),
+  name: z.string(),
+  intro: z.string(),
+  belong: z.string(),
+  coverUrl: z.string(),
+  coverDeUrl: z.string(),
+  songs: z.array(SongSummarySchema),
+});
 
-export type AlbumsAPIResponse = {
-  code: number;
-  msg: string;
-  data: Album[];
-};
+export const SongSchema = z.object({
+  cid: z.string(),
+  name: z.string(),
+  albumCid: z.string(),
+  sourceUrl: z.string(),
+  lyricUrl: z.string().nullable(),
+  mvUrl: z.string().nullable(),
+  mvCoverUrl: z.string().nullable(),
+  artists: z.array(z.string()),
+});
 
-export type AlbumDetailsAPIResponse = {
-  code: number;
-  msg: string;
-  data: AlbumDetails;
-};
+export const AlbumsAPIResponseSchema = z.object({
+  code: z.number(),
+  msg: z.string(),
+  data: z.array(AlbumSchema),
+});
 
-export type SongDetailsAPIResponse = {
-  code: number;
-  msg: string;
-  data: Song;
-};
+export const AlbumDetailsAPIResponseSchema = z.object({
+  code: z.number(),
+  msg: z.string(),
+  data: AlbumDetailsSchema,
+});
+
+export const SongDetailsAPIResponseSchema = z.object({
+  code: z.number(),
+  msg: z.string(),
+  data: SongSchema,
+});
+
+export type Album = z.infer<typeof AlbumSchema>;
+export type SongSummary = z.infer<typeof SongSummarySchema>;
+export type AlbumDetails = z.infer<typeof AlbumDetailsSchema>;
+export type Song = z.infer<typeof SongSchema>;
+export type AlbumsAPIResponse = z.infer<typeof AlbumsAPIResponseSchema>;
+export type AlbumDetailsAPIResponse = z.infer<
+  typeof AlbumDetailsAPIResponseSchema
+>;
+export type SongDetailsAPIResponse = z.infer<
+  typeof SongDetailsAPIResponseSchema
+>;
