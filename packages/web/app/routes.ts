4304bb0c530aad4faa10aac6173f2df5bc2fd7d6
happo31 <happo31@gmail.com>
2025-02-02 15:56:56 +0000

    fix: 一覧ページは _index に移動

--- packages/web/app/routes.ts
+++ packages/web/app/routes.ts
@@ -3,7 +3,6 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes";
 const routes = [
   index("routes/_index.tsx"),
   route("album/details/:cid", "routes/album.details.$cid.tsx"),
-  route("albums", "routes/albums.tsx"),
 ] satisfies RouteConfig;
 
 export default routes;
