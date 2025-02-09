706af2e16f583330c2023f91ecc94652ae142a7e
happo31 <happo31@gmail.com>
2025-02-09 15:03:19 +0000

    fix: trpc -> init

--- packages/core/src/trpc/init.ts
+++ packages/core/src/trpc/init.ts
@@ -2,5 +2,6 @@ import { initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
 
-export const router = t.router;
 export const publicProcedure = t.procedure;
+
+export const { createCallerFactory, router } = t;
